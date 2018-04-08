16bf9bca9afb8667eec15c279a8f8f9fc7a9f232
kamil-filip <kamilkozlowski89@gmail.com>
2018-04-08 11:25:17 +0200

    last assgiment

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+ 
+        $("#carousel-button").click(function(){
+            if($("#carousel-button").children("span").hasClass("fa-pause")){
+                $("#mycarousel").carousel('pause');
+                $("#carousel-button").children("span").removeClass("fa-pause");
+                $("#carousel-button").children("span").addClass("fa-play");
+            }
+            else if($("#carousel-button").children("span").hasClass("fa-play")){
+                $("#mycarousel").carousel('cycle');
+                $("#carousel-button").children("span").removeClass("fa-play");
+                $("#carousel-button").children("span").addClass("fa-pause");
+            
+            }                                                                            
+        });
+
+        $("#spanButton").click(function()
+        {
+            $("#loginModal").modal("show");
+        });
+        
+        $("#reserveTable").click(function()
+        {
+            $("#reserveModal").modal("show");
+        });
+      
+    });
